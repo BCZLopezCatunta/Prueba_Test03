7492bb1a71f9e14baa32e288020792cd41d9db05
BCZLopezCatunta <bl2020068946@virtual.upt.pe>
2024-07-11 17:23:55 -0500

    Pruebas Test 1.7

--- vendor/mockery/mockery/library/Mockery/CountValidator/Exact.php
+++ vendor/mockery/mockery/library/Mockery/CountValidator/Exact.php
@@ -1,18 +1,8 @@
 <?php
 
-/**
- * Mockery (https://docs.mockery.io/)
- *
- * @copyright https://github.com/mockery/mockery/blob/HEAD/COPYRIGHT.md
- * @license https://github.com/mockery/mockery/blob/HEAD/LICENSE BSD 3-Clause License
- * @link https://github.com/mockery/mockery for the canonical source repository
- */
-
 namespace Mockery\CountValidator;
 
 use Mockery\Exception\InvalidCountException;
-
-use const PHP_EOL;
 
 class Exact extends CountValidatorAbstract
 {
@@ -27,21 +17,21 @@ class Exact extends CountValidatorAbstract
     public function validate($n)
     {
         if ($this->_limit !== $n) {
-            $because = $this->_expectation->getExceptionMessage();
-
             $exception = new InvalidCountException(
                 'Method ' . (string) $this->_expectation
                 . ' from ' . $this->_expectation->getMock()->mockery_getName()
                 . ' should be called' . PHP_EOL
                 . ' exactly ' . $this->_limit . ' times but called ' . $n
                 . ' times.'
-                . ($because ? ' Because ' . $this->_expectation->getExceptionMessage() : '')
+                . ($this->_expectation->getExceptionMessage() ? ' Because ' . $this->_expectation->getExceptionMessage() : '')
             );
+
             $exception->setMock($this->_expectation->getMock())
                 ->setMethodName((string) $this->_expectation)
                 ->setExpectedCountComparative('=')
                 ->setExpectedCount($this->_limit)
                 ->setActualCount($n);
+
             throw $exception;
         }
     }
